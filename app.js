420b5ff13ed979d2f8a19706e3a4a2044208a6ef
xajayanthi <j.velusamy@xearts.jp>
2019-07-29 16:38:03 +0900

    サンプルアプリ

---
+++ app.js
@@ -0,0 +1,43 @@
+var items = [
+  {
+    name: '鉛筆',
+    price: 300,
+    quantity: 0
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 0
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 0
+  }
+]
+var vm = new Vue({
+  el: '#app',
+  data: {
+    items: items
+  },
+  filters: {
+    numberWithDelimiter: function (value) {
+      if (!value) {
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  computed: { // 算出プロパティ
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function () {
+      // 算出プロパティに依存した算出プロパティも定義できる
+      return Math.floor(this.totalPrice * 1.08)
+    }
+  }
+})
+window.vm = vm
